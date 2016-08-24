6f6db554f9cdf84ccba08c68106c1dd9bab458b5
Amar Prakash Pandey <amar.om1994@gmail.com>
2016-08-24 20:13:43 +0530

    game design in progress

--- carGame.py
+++ carGame.py
@@ -3,7 +3,6 @@
 # @Date:  
 # @Email: amar.om1994@gmail.com  
 # @Github username: @amarlearning
-# @Last Modified by: Amar Prakash Pandey  
 # @Last Modified date:  
 # MIT License. You can find a copy of the License
 # @http://amarlearning.mit-license.org
@@ -18,12 +17,23 @@ from os import path
 pygame.init()
 
 # constants are defined
-display_width = 600
+display_width = 700
 display_height = 600
 
+grass_width = 170
+grass_height = 600
+
+border_width = 30
+border_height = 600
+
 # Material color init
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+darkBlue = (0,0,128)
 white = (255,255,255)
 black = (0,0,0)
+grey = (211,211,211)
 
 # Folder path init
 assets = path.join(path.dirname(__file__), 'assets')
@@ -35,6 +45,7 @@ gameIcon = pygame.image.load(path.join(assets + '/gameicon.png'))
 # Game windown, caption initialised
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('SmartCar')
+
 pygame.display.update()
 
 # Game icon init
@@ -49,7 +60,14 @@ while gameplay:
 		else:
 			print event
 
-	gameDisplay.fill(white)
+	# Game basic design init [Left side]
+	pygame.draw.rect(gameDisplay, green, (0, 0, grass_width, grass_height))
+	pygame.draw.rect(gameDisplay, grey, (grass_width, 0, border_width, border_height))
+
+	# Game basic design init [Right side]
+	pygame.draw.rect(gameDisplay, green, (display_width - grass_width, 0, grass_width, grass_height))
+	pygame.draw.rect(gameDisplay, grey, (display_width - grass_width - border_width, 0, border_width, border_height))
+
 	pygame.display.update()
 
 
